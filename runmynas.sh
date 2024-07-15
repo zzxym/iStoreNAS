ef9388c136a9eda82ecb291c73140c20f91e0440
janson <janson@gmail.com>
2024-07-15 12:01:13 +0800

    better for test

--- runmynas.sh
+++ runmynas.sh
@@ -19,9 +19,17 @@ CURR=`pwd`
 mkdir -p dl
 mkdir -p ${CURR}/ib_${TARGET}
 
-docker run -it --rm -u $(id -u):$(id -g) \
-  -v ${CURR}:/work \
-  -v ${CURR}/ib_${TARGET}:/work/ib \
-	-e WORK_TARGET=${TARGET} \
-  linkease/runmynas:latest
+if [ "$PACK" = "1" ]; then
+  docker run -it --rm -u $(id -u):$(id -g) \
+    -v ${CURR}:/work \
+    -v ${CURR}/ib_${TARGET}:/work/ib \
+    -e WORK_TARGET=${TARGET} \
+    linkease/runmynas:latest Pack
+else
+  docker run -it --rm -u $(id -u):$(id -g) \
+    -v ${CURR}:/work \
+    -v ${CURR}/ib_${TARGET}:/work/ib \
+    -e WORK_TARGET=${TARGET} \
+    linkease/runmynas:latest Pack
+fi
 
